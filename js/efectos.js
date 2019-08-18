3a80035e127b5e7fcd9bf64a34adbd6e8a53ff75
YouSay <yousay.sd@gmail.com>
2019-08-18 18:59:48 -0300

    Subiendo proyecto Restaurante con JQuery

---
+++ js/efectos.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+	
+// EFECTO MENU
+
+	$('.menu a').each(function(index, elemento){
+		$(this).css({
+			'top': '-200px'
+		});
+
+		$(this).animate({
+			top: '0'
+		}, 2000 + (index * 500));
+	});
+
+
+// EFECTO HEADER
+	if( $(window).width() > 800){
+		$('header .textos').css({
+			opacity: 0,
+			marginTop: 0
+		});
+
+		$('header .textos').animate({
+			opacity: 1,
+			marginTop: '-52px'
+		}, 1500);
+	}
+
+// SCROLL ELEMENTOS MENU
+
+	var acercaDe = $('#acerca-de').offset().top,
+		menu = $('#platillos').offset().top,
+		galeria = $('#galeria').offset().top,
+		ubicacion = $('#ubicacion').offset().top;
+
+
+	$('#btn-acerca-de').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: acercaDe - 100
+		}, 500);
+	});
+
+	$('#btn-menu').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: menu
+		}, 500);
+	});
+
+	$('#btn-galeria').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: galeria
+		}, 500);
+	});
+
+	$('#btn-ubicacion').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: ubicacion
+		}, 500);
+	});
+});
+
